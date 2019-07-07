54e4da371fa61d3d469f2b6ced9d6a9b196514f9
Marcel Mueller <muellerphysics@gmail.com>
2019-07-07 22:22:12 +0200

    Added basic functions to IDS support

--- bridgelibs/cam_connect_ids.c
+++ bridgelibs/cam_connect_ids.c
@@ -4,20 +4,26 @@
 #include <ueye.h>
 #include "org_mueller_physics_camera_connect_CameraConnect_IDS.h"
 
+struct {
+
+    char *  ppcImgMem;
+    int	    pid;
+
+} ids_mem_info ;
+
+
 
 // Allow to throw an exception if IDS reports anything but success
-void errcheck( JNIEnv * env, int res, const char * const errMsg ){
+void errcheck( JNIEnv * env, int hCam, int res ){
     if (res != IS_SUCCESS) {
-	 
-	 // TODO: throw proper custom exception
-	 
-	 //(*env)->ThrowNew( env, (*env)->FindClass(env, "org/mueller_physics/camera_connect/ApiError"), errMsg);
+	
+	char * errMsg;
+	is_GetError( hCam, &res, &errMsg);
 
+	// TODO: throw proper custom exception
+	 //(*env)->ThrowNew( env, (*env)->FindClass(env, "org/mueller_physics/camera_connect/ApiError"), errMsg);
 	/*(*env)->CallStaticVoidMethod( env, 
 	    (*env)->FindClass(env,"org/mueller_physics/camera_connect/ApiError"), 
-
-
-
 	 jclass  cls = (*env)->FindClass(env, "org/mueller_physics/camera_connect/ApiError");
 	 jmethod mtd = (*env)->GetStaticMethodID(env, cls, "throwApiError", "(I)V"); */
 
@@ -30,21 +36,103 @@ void errcheck( JNIEnv * env, int res, const char * const errMsg ){
 
 
 // query number of cameras
-JNIEXPORT jint JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1getNrCams
+JNIEXPORT jint JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1GetNumberOfCameras
   (JNIEnv *env, jclass c) {
 
     int res, nrCams;
     res = is_GetNumberOfCameras(&nrCams);
-    errcheck( env, res, "IDS internal error");
+    errcheck( env, 0, res);
     return nrCams;
-  }
+}
 
 
 // initialize camera
-JNIEXPORT jint JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1initCam
+JNIEXPORT jint JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1InitCamera
   (JNIEnv *env, jclass c, jint camhd ) {
     
-    errcheck( env, is_InitCamera( &camhd, NULL), "Failed to connect to camera");
+    errcheck( env, camhd, is_InitCamera( &camhd, NULL));
     return 0;
+}
+
+
+// release the camera
+JNIEXPORT void JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1ExitCamera
+  (JNIEnv *env, jclass c, jint  camhd) {
+
+    errcheck( env, camhd, is_ExitCamera( camhd ));
+
+}
+
+
+// allocate image memory. 
+// returns pointers and id as a java array
+JNIEXPORT jlongArray JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1AllocImageMem
+  (JNIEnv *env, jclass c, jint camhd, jint width, jint height, jint bpp) {
+   
+    jlongArray res = (*env)->NewLongArray(env, 2);
+    if (res==NULL) {
+	(*env)->ThrowNew(env, (*env)->FindClass(env, "java/lang/Exception"), "mem alloc failed");
     }
+
+    char * loc; int pid;
+
+    errcheck( env, camhd, is_AllocImageMem( camhd, width, height, bpp, &loc, &pid)); 
+
+    jlong ret[2] = { (long)loc, pid };
+
+    (*env)->SetLongArrayRegion(env, res, 0, 2, ret);
+    return res;
+
+}
+
+
+// ---- Pixel clocks, exposure times, etc. ----
+
+// return list of avail. pixel clocks
+JNIEXPORT jintArray JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1PixelClockGetList
+  (JNIEnv *env, jclass c, jint camhd ) {
+
+    int nrPxlClk;
+    errcheck( env, camhd, is_PixelClock(camhd, IS_PIXELCLOCK_CMD_GET_NUMBER, &nrPxlClk, sizeof(nrPxlClk)));
+
+    jintArray res = (*env)->NewIntArray(env, nrPxlClk);
+    if (res==NULL) {
+	(*env)->ThrowNew(env, (*env)->FindClass(env, "java/lang/Exception"), "mem alloc failed");
+    }
+
+    if (nrPxlClk==0) {
+	return res;
+    }
+
+    UINT pxlClocks[nrPxlClk];
+    errcheck( env, camhd, is_PixelClock(camhd, IS_PIXELCLOCK_CMD_GET_LIST, &pxlClocks, sizeof(UINT)*nrPxlClk));
+
+    jint ret[nrPxlClk];
+    for (int i=0;i<nrPxlClk;i++) {
+	ret[i] = pxlClocks[i];
+    }
+
+    (*env)->SetIntArrayRegion(env, res, 0, nrPxlClk, ret);
+    return res;
+
+}
+
+// set framerate
+JNIEXPORT jdouble JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1FrameRateSet
+  (JNIEnv *env, jclass c, jint camhd, jdouble fps) {
+    double newfps;
+    errcheck(env, camhd, is_SetFrameRate(camhd, fps, &newfps));
+    return newfps;
+}
+
+// query framerate
+JNIEXPORT jdouble JNICALL Java_org_mueller_1physics_camera_1connect_CameraConnect_1IDS_idsj_1FrameRateQuery
+  (JNIEnv *env, jclass c, jint camhd) {
+    double newfps;
+    errcheck(env, camhd, is_SetFrameRate(camhd, IS_GET_FRAMERATE, &newfps));
+    return newfps;
+}
+
+
+
 
--- org/mueller_physics/camera_connect/CameraConnect_IDS.java
+++ org/mueller_physics/camera_connect/CameraConnect_IDS.java
@@ -1,4 +1,3 @@
-
 /*
 This file is part of the IR focus tracker (ir-track). 
 
@@ -40,15 +39,18 @@ public class CameraConnect_IDS {
 
     public CameraConnect_IDS() {};
 
-    public CameraConnect_IDS(int nr) {
-	connect(nr);
-    }
-
     /** connect to camera #n **/
-    public void connect(int camId) {
-	idsj_initCam(camId);
+    public void connect(int id) {
+	idsj_InitCamera(id);
+	camhd=id;
     }
 
+    /** connect from camera */
+    public void disconnect() {
+	if (camhd<0) return;
+	idsj_ExitCamera(camhd);
+	camhd=-1;
+    }
 
 
 
@@ -57,20 +59,44 @@ public class CameraConnect_IDS {
     // --- JNI native calls ----
 
 
-    /** returns the total number of connected IDS cameras */
-    static native int idsj_getNrCams();
+    /** Returns the total number of connected IDS cameras.
+     *  calls 'ids_GetNumberOfCameras() */
+    private static native int idsj_GetNumberOfCameras();
 
-    static native int idsj_initCam(int camId);
+    /** Initializes an IDS camera based on ID.
+     *  calls 'ids_InitCamera(camId)'
+     *	@param camera_id */
+    private static native int idsj_InitCamera(int hCam);
     
+    /** Allocates image memory.
+     *  calls 'is_AllocImageMemory'.
+     *  @return A long[] with 'ppcImgMem' and 'pid' */
+    private static native long[] idsj_AllocImageMem(int hCam, int w, int h, int bpp);
+
+    /** Exits an IDS camera based on ID.
+     *  calls 'ids_ExitCamera(camId)' */
+    private static native void idsj_ExitCamera(int hCam);
+
+
+    /** Get a list of supported pixel clocks.
+     *  calls is_PixelClock(). TODO: Handle cameras with continuous pixel clock settings. 
+     *  These will currently return an empty list of size 0. */
+    private static native int[] idsj_PixelClockGetList(int hCam);
+    
+    /** Set a new camera framerate-
+     * calls 'is_SetFrameRate()'.
+     * @return the new framerate */
+    private static native double idsj_FrameRateSet(int hCam, double fps);
+
+    /** Query the cameras currently set frame rate */
+    private static native double idsj_FrameRateQuery(int hCam);
 
 
     // --- test the class ---
-
-
     public static void main(String [] args) {
 
 
-	int nrCams = idsj_getNrCams();
+	int nrCams = idsj_GetNumberOfCameras();
 	System.out.println("IDS cameras: "+nrCams);
 	
 	if (nrCams <1) {
@@ -78,10 +104,36 @@ public class CameraConnect_IDS {
 	    return;
 	}
 
-	CameraConnect_IDS idsConn = new CameraConnect_IDS(1);
+	// run low level (static jni calls) functions
+	int id=1;			// camera id to use
+	int rx=16,ry=32,rw=640,rh=480;	// parameters for ROI setting
+	
+	
+	System.out.println("--- API level functions ----");
 
-	System.out.println("connected");
-	System.out.flush();
+	idsj_InitCamera(id);
+	System.out.println("connected to camera: "+id);
+
+
+	int [] pxlClocks = idsj_PixelClockGetList(id);
+	for (int i=0; i<pxlClocks.length; i++) {
+	    System.out.println("pixel clock option ["+i+"]: "+pxlClocks[i]);
+	}
+
+	double fps = idsj_FrameRateQuery(id);
+	System.out.println("current fps: "+fps);
+
+	fps = idsj_FrameRateSet(id,10.);
+	System.out.println("Frame rate set 10 fps, now: "+fps);
+
+
+	long [] res = idsj_AllocImageMem(id, rw, rh, 16);
+	System.out.println("allocated memory: pid: "+res[1]+" loc: "+res[0]);
+
+	idsj_ExitCamera(id);
+	System.out.println("disconnected");
+	
+
 
     }
 
