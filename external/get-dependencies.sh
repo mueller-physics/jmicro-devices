623407006d8ef78e76d5018ff387e17372c25c8f
Marcel Müller <muellerphysics@gmail.com>
2019-07-11 07:18:37 +0200

    Added MicroManager jars, corrected license headers

--- external/get-dependencies.sh
+++ external/get-dependencies.sh
@@ -17,6 +17,7 @@ failcmd wget
 failcmd ar
 failcmd xz
 failcmd tar
+failcmd 7z
 
 # Get the ImageJ base library (in version 1.48, which is the lowest we currently support)
 if [ ! -e ij148v.jar ] ; then
@@ -67,6 +68,36 @@ if [ ! -e rt-1.6.jar ] ; then
 else
     echo "found Java 1.6 runtime"
 fi
+
+
+# This extracts the MicroManager jars we need to compile the camera
+# plugin. It requires '7z' from p7zip to be installed
+if [ ! -e MMCoreJ.jar -o ! -e MMJ_.jar ] ; then
+    fileMissing=1
+
+    # downloading the micromanager install dmg (easier to handle than the exe)
+    wget -c "http://valelab4.ucsf.edu/~MM/builds/1.4/Mac/Micro-Manager1.4.22.dmg"
+
+    mkdir tmp-mm-jar
+    cd tmp-mm-jar
+
+    # extract the dmg file
+    7z x ../Micro-Manager1.4.22.dmg
+    
+    # unpack the files from the image 
+    7z x 2.hfs
+    
+    # 'MMCoreJ.jar' and 'MMJ_.jar' from that exe file
+    cp Micro-Manager/Micro-Manager1.4/plugins/Micro-Manager/MMCoreJ.jar ../
+    cp Micro-Manager/Micro-Manager1.4/plugins/Micro-Manager/MMJ_.jar ../
+
+    # delete all the unused stuff
+    cd ..
+    rm -rf tmp-mm-jar
+else
+    echo "found MicroManager-jars"
+fi
+
 
 
 if [ $fileMissing -eq 0 ] ; then
--- org/mueller_physics/device_gui/CameraSimpleGUI.java
+++ org/mueller_physics/device_gui/CameraSimpleGUI.java
@@ -1,19 +1,21 @@
 /*
-This file is part of the IR focus tracker (ir-track). 
+This file is part of the jmicro-devices. 
 
-ir-track is free software: you can redistribute it and/or modify
+jmicro-devices is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
-ir-track is distributed in the hope that it will be useful,
+jmicro-devices is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with ir-track.  If not, see <http://www.gnu.org/licenses/>
+along with jmicro-devices.  If not, see <http://www.gnu.org/licenses/>
 */
+
+
 package org.mueller_physics.device_gui;
 
 import org.mueller_physics.device_connect.CameraConnect_IDS;
