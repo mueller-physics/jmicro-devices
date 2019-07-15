c1f0310d1a59f15c0cd22a51bd80fba6aa85d315
Marcel Müller <muellerphysics@gmail.com>
2019-07-15 20:56:39 +0200

    Demo camera can run z-stacks connected to stage

--- org/mueller_physics/device_connect/CameraConnect_Dummy.java
+++ org/mueller_physics/device_connect/CameraConnect_Dummy.java
@@ -18,6 +18,7 @@ along with jmicro-devices.  If not, see <http://www.gnu.org/licenses/>
 package org.mueller_physics.device_connect;
 
 import org.mueller_physics.devices.JMicroCamera;
+import org.mueller_physics.devices.JMicroLinearMover;
 
 import java.util.Random;
 
@@ -29,11 +30,19 @@ public class CameraConnect_Dummy
     implements JMicroCamera {
 
 
-    ImageStack demoStack = null;
+    ImageStack [] demoStacks = null;
 
     double curExp=5;
+
+    int demoStackImg = 0;
     int demoStackPos = 0;
     boolean demoStackAutoInc;
+
+    int zOffset = 0;
+    double zTwiddle = 0;
+    double zSpacing = 0;
+    JMicroLinearMover zStage = null;
+
 
     double incX, incY;
 
@@ -42,23 +51,47 @@ public class CameraConnect_Dummy
     int roiW, roiH, roiX, roiY;
 
     Random prng = new Random(2342);
-    
+
+    /** Create a dummy camera returning noise.
+     *  Exposure time on this camera will set photon count.
+     *  Shot noise and some structure is added to the image.
+     *  @param w Width of the dummy sensor
+     *  @param h Height of the dummy sensor */
     public CameraConnect_Dummy(int w, int h) {
 	sensor_w=w; sensor_h=h;
 	roiX=roiY=0;
 	roiW=w; roiH=h;
     }
     
-    public CameraConnect_Dummy(ImageStack is ) {
-	demoStack=is;
-	sensor_w = is.getWidth();
-	sensor_h = is.getHeight();
+
+    /** Create a dummy camera playing back image stack(s).
+     *  On 'snapImage', an image from the stack will be returned.
+     *  Auto increment is possible (simulating time lapse) as well
+     *  as linking to a stage position (simulating z-stepping).
+     *  Multiple (same size!) stacks can be passed and switched through
+     *  to simulate switch in xy-position.
+     *  @param is Image Stack(s) of the same size (width, height) */
+    public CameraConnect_Dummy(ImageStack ... is ) {
+	demoStacks=is;
+    
+	if (is==null || is.length==0) {
+	    throw new NullPointerException("stack array null or empty");
+	}
+	sensor_w = is[0].getWidth();
+	sensor_h = is[0].getHeight();
+	
+	for (int i=0;i<is.length; i++) {
+	    if ( is[i].getWidth() != sensor_w || is[i].getHeight() != sensor_h ) {
+		throw new RuntimeException("stack "+i+" not the same dimensions");
+	    }
+	}
 	roiX=roiY=0;
 	roiW=sensor_w; roiH=sensor_h;
 	demoStackAutoInc = true;
     }
     
 
+    /** Set the exposure time of the camera */
     @Override
     public double setExposureTime(double e) {
 	curExp = e*0.995;
@@ -91,7 +124,7 @@ public class CameraConnect_Dummy
 	    
 	short [] ret = new short[roiW*roiH];
 	
-	if ( demoStack == null ) {
+	if ( demoStacks == null ) {
 	    for (int y=0; y<roiH; y++) {
 		for (int x=0; x<roiW; x++) {
 		     
@@ -101,7 +134,7 @@ public class CameraConnect_Dummy
 		    
 		    val += Math.sqrt(val)*prng.nextGaussian();
 		    
-		    ret[x+sensor_w*y] = (short)val;
+		    ret[x+roiW*y] = (short)val;
 
 		}
 	    }
@@ -109,23 +142,48 @@ public class CameraConnect_Dummy
 	    incY++;
 	} else {
 
-	    ImageProcessor ip = demoStack.getProcessor( 
-		((demoStackPos)%demoStack.getSize())+1);
+	    // if we have a stage, calc the image pos
+	    if ( zStage != null ) {
+		double pos = zStage.getPosition() + zTwiddle;
+		demoStackImg = (int)(pos / zSpacing) + zOffset;
+	    }
+
+	    int pos = (demoStackImg%demoStacks[demoStackPos].getSize());
+	    while (pos<0) { pos += demoStacks[demoStackPos].getSize(); };
+
+	    ImageProcessor ip = demoStacks[demoStackPos].getProcessor( pos+1 );
 
 	    for (int y=0; y<roiH; y++) {
 		for (int x=0; x<roiW; x++) {
-		    ret[x+roiW*y] = (short)ip.getf(x+roiX, y+roiY);
+		    ret[x+roiW*y] = (short)ip.get(x+roiX, y+roiY);
 		}
 	    }
 	    if (demoStackAutoInc) {
-		demoStackPos++;
+		demoStackImg++;
 	    }
 	}
 	
 	return ret;
     }
 
+    /** Determine which image is returned by stage position.
+     * This allows to simulate z-stacking. Every time an image is taken,
+     * 'getPosition' is issued to the stage, and converted into the image returned.
+     * @param zOffset  Which slice in the stack is at stage pos 0
+     * @param zSpacing Microns between slices in the stack 
+     * @param zStage The stage to query for position*/
+    public void linkStage( int zOffset, double zSpacing, JMicroLinearMover zStage ) {
 
+	this.zOffset  = zOffset;
+	this.zSpacing = zSpacing;
+	this.zStage   = zStage;
+	demoStackAutoInc = false;
+
+    }
+
+    public void setStageTwiddle( double m ) {
+	this.zTwiddle = m;
+    }
 
 }
 
